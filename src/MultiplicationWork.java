d657b93d4220b508e1e9727a418d9ed5ce3c1f50
Frank G <s1226135@monmouth.edu>
2020-02-27 20:04:29 -0500

    Version 1

---
+++ src/MultiplicationWork.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class MultiplicationWork 
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.printf("     ");
+		for (int x = 1; x <= 10; x++) 
+		{
+
+			System.out.printf("%5d",x);
+		}
+		System.out.println();
+		System.out.println("_______________________________________________________");
+		for (int i = 1; i <= 10; i++) 
+		{
+			System.out.printf("%2d | ",i);
+			for (int a = 1; a <= 10; a++) 
+			{
+				System.out.printf("%5d", (a * i));
+			}
+			System.out.println();
+		}
+
+	}
+
+}
