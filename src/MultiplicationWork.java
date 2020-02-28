0cbcba170255d823220b705ef6789cbabb8608ef
fgleeson27 <55250949+fgleeson27@users.noreply.github.com>
2020-02-27 20:11:38 -0500

    Version 1

--- src/MultiplicationWork.java
+++ src/MultiplicationWork.java
@@ -1,23 +1,20 @@
  
-public class MultiplicationWork 
-{
+public class MultiplicationWork {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 
 		System.out.printf("     ");
-		for (int x = 1; x <= 10; x++) 
-		{
+		for (int x = 1; x <= 10; x++) {
 
 			System.out.printf("%5d",x);
 		}
 		System.out.println();
 		System.out.println("_______________________________________________________");
-		for (int i = 1; i <= 10; i++) 
-		{
+		for (int i = 1; i <= 10; i++) {
+			
 			System.out.printf("%2d | ",i);
-			for (int a = 1; a <= 10; a++) 
-			{
+			for (int a = 1; a <= 10; a++) {
+				
 				System.out.printf("%5d", (a * i));
 			}
 			System.out.println();
